a3739780db94e1bee25a7b8af1d0256fdf21725f
ivashkov <alexey.ivashkov@folium.cloud>
2022-03-12 17:41:17 +0300

    Changes after CR

--- sources/rmq/modules/cmn-rabbitmq/src/main/java/net/its/rmq/cmn/rabbitmq/publisher/DefaultMessagePublisher.java
+++ sources/rmq/modules/cmn-rabbitmq/src/main/java/net/its/rmq/cmn/rabbitmq/publisher/DefaultMessagePublisher.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import net.its.rmq.cmn.rabbitmq.pool.RabbitmqChannelPool;
 
+import java.io.IOException;
+
 @Slf4j
 @RequiredArgsConstructor
 public class DefaultMessagePublisher implements MessagePublisher {
@@ -12,14 +14,11 @@ public class DefaultMessagePublisher implements MessagePublisher {
     private final RabbitmqChannelPool channelPool;
 
     @Override
-    public void publish(String exchange, String routingKey, byte[] message) {
+    public void publish(String exchange, String routingKey, byte[] message) throws IOException {
 
-        try {
-            val channel = channelPool.getChannel();
-            channel.basicPublish(exchange, routingKey, null, message);
-            channelPool.releaseChannel(channel);
-        } catch (Exception ex) {
-            log.error("Unable to publish message, exchange " + exchange + ", routingKey: " + routingKey, ex);
-        }
+        val channel = channelPool.getChannel();
+        channel.basicPublish(exchange, routingKey, null, message);
+        channelPool.releaseChannel(channel);
+
     }
 }
--- sources/rmq/modules/cmn-rabbitmq/src/main/java/net/its/rmq/cmn/rabbitmq/publisher/MessagePublisher.java
+++ sources/rmq/modules/cmn-rabbitmq/src/main/java/net/its/rmq/cmn/rabbitmq/publisher/MessagePublisher.java
@@ -1,6 +1,8 @@
 package net.its.rmq.cmn.rabbitmq.publisher;
 
+import java.io.IOException;
+
 public interface MessagePublisher {
 
-    void publish(String exchange, String routingKey, byte[] message);
+    void publish(String exchange, String routingKey, byte[] message) throws IOException;
 }
