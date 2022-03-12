21555207fec76c6453da81b9261541b0b2bbadf4
ivashkov <alexey.ivashkov@folium.cloud>
2022-03-12 12:39:40 +0300

    PathVariable -> RequestBody

--- sources/rmq/app-incoming-handler/src/main/java/net/its/rmq/incoming/web/controller/IncomingController.java
+++ sources/rmq/app-incoming-handler/src/main/java/net/its/rmq/incoming/web/controller/IncomingController.java
@@ -7,8 +7,8 @@ import lombok.val;
 import net.its.rmq.incoming.exception.IncomingServiceException;
 import net.its.rmq.incoming.service.IncomingService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +19,9 @@ public class IncomingController {
     private final IncomingService publisher;
     private final ObjectMapper objectMapper;
 
-    @PostMapping("/incoming/{text}")
+    @PostMapping("/incoming")
     @ResponseStatus(HttpStatus.OK)
-    public void getIncoming(@PathVariable("text") String text) {
+    public void getIncoming(@RequestBody String text) {
 
         try {
             val message = objectMapper.writeValueAsBytes(text);
