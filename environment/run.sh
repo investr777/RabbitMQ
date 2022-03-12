382f2ac6863b8cd187b1ae2a8d393ecefa440a48
Maksim Zhuridov <maks.juridov@folium.cloud>
2022-03-12 15:30:33 +0300

    Add run script

---
+++ environment/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ENVIRONMENT_DIR=$PWD
+
+cd ../sources/rmq/
+./gradlew clean build
+
+cd $ENVIRONMENT_DIR
+
+docker-compose -f docker-compose.yaml build
+docker-compose -f docker-compose.yaml up -d
